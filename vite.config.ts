642138797300521b0658435fa0e77d06f20b78fb
Aleh Pilets <pilets.oleg@gmail.com>
2025-11-26 23:22:11 +0100

    Fix vite config and add basee

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 import { qrcode } from 'vite-plugin-qrcode'
 
 
@@ -8,7 +7,6 @@ import { qrcode } from 'vite-plugin-qrcode'
 export default defineConfig({
   plugins: [
 		react(),
-		tailwindcss(),
 		qrcode() 
 	],
 	base: "/kotra/",
