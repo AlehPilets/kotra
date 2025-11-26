4c1cb05e7f00cbb8158a3a83c2005ceea1d2eabc
Aleh Pilets <pilets.oleg@gmail.com>
2025-11-26 22:38:12 +0100

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import { qrcode } from 'vite-plugin-qrcode'
+
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+		tailwindcss(),
+		react(),
+		qrcode() 
+	],
+	base: "/kotra/",
+	build: {
+    chunkSizeWarningLimit: 1000
+  },
+	server: {
+    port: 5175, 
+  },
+})
