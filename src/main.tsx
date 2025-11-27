f8911e08850f9ac416b776d1286ad2b5321be315
Aleh Pilets <pilets.oleg@gmail.com>
2025-11-27 11:45:14 +0100

    Fix vite config and add basee

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from './App.tsx'
 import { HeroUIProvider } from '@heroui/react'
 import { BrowserRouter } from 'react-router-dom'
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <HeroUIProvider>
       <BrowserRouter basename="/kotra">
